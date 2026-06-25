416ab4f0d9ca9db5724c93db2cbf216e0c7b27d3
kovs713 <ovsyannikovkostyan@gmail.com>
2026-06-26 00:53:03 +0400

    feat: add s3 integration

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,6 @@
 import { Bot, Context } from "grammy";
 import type { Message, PhotoSize } from "grammy/types";
-
-declare module "bun" {
-  interface Env {
-    BOT_TOKEN: string;
-  }
-}
+import { client } from "./s3_client";
 
 const COOL_STICKER_FILE_ID =
   "CAACAgIAAxkBAAMGaj15VEVjJkj-ad3gjwjG1sv8OhQAAshMAAJ35mFI7mejcmcukIc8BA";
@@ -14,27 +9,34 @@ const bot = new Bot(process.env.BOT_TOKEN);
 bot.on("message", async (ctx: Context) => {
   const message = ctx.message as Message;
   const userId = message.from?.id;
+  if (!userId) return;
 
   // telegram send array of different photo sizes
-  const photoArray: PhotoSize[] = message.photo ?? [];
+  const photoArray = message.photo;
+  if (photoArray) {
+    const best_photo = photoArray.at(-1) as PhotoSize;
+    if (best_photo) {
+      const file = await ctx.api.getFile(best_photo.file_id);
 
-  if (userId) {
-    if (message.sticker) {
-      await bot.api.sendMessage(userId, "мой стикер круче далбаеб");
-      ctx.replyWithSticker(COOL_STICKER_FILE_ID);
-    }
-
-    if (photoArray) {
-      const largest_photo = photoArray.at(-1) as PhotoSize;
-      if (largest_photo) {
-        // ctx.replyWithPhoto(largest_photo.file_id);
-        await bot.api.sendPhoto(userId, largest_photo.file_id, {
-          caption: "ну и говно",
-        });
-      } else {
-        await bot.api.sendMessage(userId, "какая то хуйня бро");
+      const file_url = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${file.file_path}`;
+      const response = await fetch(file_url);
+      if (response.ok || response.body) {
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        await client.write(`test/говно-${best_photo.file_id}`, buffer);
       }
+
+      await bot.api.sendPhoto(userId, best_photo.file_id, {
+        caption: "ну и говно",
+      });
+    } else {
+      await bot.api.sendMessage(userId, "какая то хуйня бро");
     }
+  }
+
+  if (message.sticker) {
+    await bot.api.sendMessage(userId, "мой стикер круче далбаеб");
+    ctx.replyWithSticker(COOL_STICKER_FILE_ID);
   }
 });
 
