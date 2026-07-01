76f389e8c4b9f2ab5a4803f90c6e4c1aafec564a
kovs713 <ovsyannikovkostyan@gmail.com>
2026-07-01 14:47:46 +0400

    refactor: extract handlers, types and utils

---
+++ server/src/api/constants.ts
@@ -0,0 +1,5 @@
+export const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "authorization,content-type",
+  "Access-Control-Allow-Methods": "GET,POST,PATCH,OPTIONS",
+}
