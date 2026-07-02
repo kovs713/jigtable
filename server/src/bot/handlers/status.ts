3ca549d815d7aa5ed6f8483e09ff61ac2834888c
kovs713 <ovsyannikovkostyan@gmail.com>
2026-07-02 22:02:02 +0400

    feat: micro optimisation

--- server/src/bot/handlers/status.ts
+++ server/src/bot/handlers/status.ts
@@ -5,6 +5,31 @@ import type { BotContext } from "@/bot/types"
 
 let coolImageFileId: string | null = null
 
+export async function handleStatus(
+  ctx: CommandContext<BotContext>
+): Promise<void> {
+  // Session not started
+  if (!ctx.session.isStarted) {
+    await ctx.reply(
+      "ничего не начато ебаклак, нажми /new чтобы начать свой шлак кидать"
+    )
+    return
+  }
+
+  const photos = ctx.session.photos
+
+  // Else if started and images pushed tell how much
+  if (photos.length) {
+    await ctx.reply(
+      `batch ${ctx.session.activeBatchId}: сейчас в меня засовано ${photos.length} шлака`
+    )
+    return
+  }
+
+  // If not pushed
+  await replyWithCoolPhoto(ctx)
+}
+
 async function replyWithCoolPhoto(
   ctx: CommandContext<BotContext>
 ): Promise<void> {
@@ -12,11 +37,16 @@ async function replyWithCoolPhoto(
     await ctx.replyWithPhoto(coolImageFileId, {
       caption: "ничего не засовано",
     })
+    return
   }
 
   const response = await fetch(COOL_IMAGE_S3_FILE_NAME)
   if (!response.ok || !response.body) {
-    throw new Error(`failed to fetch cool asset: ${response.status}`)
+    console.warn(
+      `ERROR: Failed to fetch cool asset with status ${response.status}`
+    )
+    await ctx.reply("ничего не засовано")
+    return
   }
 
   const message = await ctx.replyWithPhoto(
@@ -27,25 +57,4 @@ async function replyWithCoolPhoto(
   )
 
   coolImageFileId = message.photo.at(-1)?.file_id ?? null
-}
-
-export async function handleStatus(
-  ctx: CommandContext<BotContext>
-): Promise<void> {
-  if (!ctx.session.isStarted) {
-    await ctx.reply(
-      "ничего не начато ебаклак, нажми /new чтобы начать свой шлак кидать"
-    )
-    return
-  }
-
-  const photos = ctx.session.photos
-  if (photos.length) {
-    await ctx.reply(
-      `batch ${ctx.session.activeBatchId}: сейчас в меня засовано ${photos.length} шлака`
-    )
-    return
-  }
-
-  await replyWithCoolPhoto(ctx)
 }
