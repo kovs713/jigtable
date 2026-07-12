22661225b8fbec2f95f819776287c5d6961bc1f6
kovs713 <ovsyannikovkostyan@gmail.com>
2026-07-12 23:35:38 +0400

    fix(server): new editor client url

--- server/src/bot/utils.ts
+++ server/src/bot/utils.ts
@@ -1,5 +1,5 @@
 export function clientLayoutUrl(compositionId: string, token: string): string {
-  const url = new URL(process.env.CLIENT_URL)
+  const url = new URL(`${process.env.CLIENT_URL}/editor`)
 
   url.searchParams.set("compositionId", compositionId)
   url.searchParams.set("token", token)
